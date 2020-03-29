e893f0ff147f4272c3e6f1bd83365fda2a2e52c8
Matheus Deon Bordignon <matheusdb_@outlook.com>
2020-03-29 00:46:10 -0300

    Mobile da Aplicação

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -38,52 +38,54 @@ export default function Register(){
     
     return (
         <div className = "register-container">
-            <section>
-                <img src = {logoImg} alt = "Be The Hero"/>
+            <div className = "content">
+                <section>
+                    <img src = {logoImg} alt = "Be The Hero"/>
 
-                <h1>Cadastro</h1>
-                <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-                <Link className = "back-link" to = "/" >
-                    <FiArrowLeft size={16} color="#E02041"/>
-                    Não tenho cadastro
-                </Link>
-            </section>
+                    <Link className = "back-link" to = "/" >
+                        <FiArrowLeft size={16} color="#E02041"/>
+                        Voltar para o Logon
+                    </Link>
+                </section>
 
-            <form onSubmit = {handleRegister}>
-                <input 
-                    placeholder = "Nome da ONG" 
-                    value = {name}
-                    onChange = {e => setName(e.target.value)} 
-                />
-                <input 
-                    type = "email" 
-                    placeholder = "E-mail"
-                    value = {email}
-                    onChange = {e => setEmail(e.target.value)} 
-                />
-                <input 
-                    placeholder = "WhatsApp" 
-                    value = {whatsapp}
-                    onChange = {e => setWhatspp(e.target.value)} 
-                />
-                
-                <div className = "input-group">
+                <form onSubmit = {handleRegister}>
                     <input 
-                        placeholder = "Cidade"
-                        value = {city}
-                        onChange = {e => setCity(e.target.value)} 
-                     />
-                    <input 
-                        placeholder = "UF" 
-                        style = {{width: 80}}
-                        value = {uf}
-                        onChange = {e => setUf(e.target.value)} 
+                        placeholder = "Nome da ONG" 
+                        value = {name}
+                        onChange = {e => setName(e.target.value)} 
                     />
-                </div>
+                    <input 
+                        type = "email" 
+                        placeholder = "E-mail"
+                        value = {email}
+                        onChange = {e => setEmail(e.target.value)} 
+                    />
+                    <input 
+                        placeholder = "WhatsApp" 
+                        value = {whatsapp}
+                        onChange = {e => setWhatspp(e.target.value)} 
+                    />
+                    
+                    <div className = "input-group">
+                        <input 
+                            placeholder = "Cidade"
+                            value = {city}
+                            onChange = {e => setCity(e.target.value)} 
+                        />
+                        <input 
+                            placeholder = "UF" 
+                            style = {{width: 80}}
+                            value = {uf}
+                            onChange = {e => setUf(e.target.value)} 
+                        />
+                    </div>
 
-                <button className = "button" type = "submit">Cadastrar</button>
-            </form>
+                    <button className = "button" type = "submit">Cadastrar</button>
+                </form>
+            </div>
         </div>
     )
 }
